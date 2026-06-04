7a45c9c669e9b706705c0c4f1f4e00caf698ced9
wang <1690703797@qq.com>
2026-06-04 05:39:50 +0000

    3.2.2

--- CI/prepare-javadocs.sh
+++ CI/prepare-javadocs.sh
@@ -4,11 +4,14 @@ CUR=$(pwd)
 TMPDIR="$(dirname -- "${0}")"
 
 SC_RELEASE_TAG="v$SC_VERSION"
+SOURCE_DIR="$CUR/modules/swagger-annotations/target/javadocprep/swagger-core/${SC_VERSION}/apidocs"
 
-#####################
-### publish javadocs
-#####################
+if [ ! -d "$SOURCE_DIR" ]; then
+  echo "Missing generated javadocs at $SOURCE_DIR"
+  find "$CUR/modules/swagger-annotations/target" -maxdepth 3 \( -type d -o -type f \) | sort
+  exit 1
+fi
 
-cp -aR $CUR/modules/swagger-annotations/target/javadocprep/swagger-core/${SC_VERSION}/apidocs $TMPDIR
-cp -a $CUR/CI/publish-javadocs.sh $TMPDIR/publish-javadocs.sh
+cp -aR "$SOURCE_DIR" "$TMPDIR"
+cp -a "$CUR/CI/publish-javadocs.sh" "$TMPDIR/publish-javadocs.sh"
 
