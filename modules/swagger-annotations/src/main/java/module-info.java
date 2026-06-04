a8b330f1b46a0589ca18f3a7aadc7986c0063710
wang <1690703797@qq.com>
2026-06-04 06:37:10 +0000

    3.3.4

---
+++ modules/swagger-annotations/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module io.swagger.v3.oas.annotations {
+    exports io.swagger.v3.oas.annotations;
+    exports io.swagger.v3.oas.annotations.callbacks;
+    exports io.swagger.v3.oas.annotations.enums;
+    exports io.swagger.v3.oas.annotations.extensions;
+    exports io.swagger.v3.oas.annotations.headers;
+    exports io.swagger.v3.oas.annotations.info;
+    exports io.swagger.v3.oas.annotations.links;
+    exports io.swagger.v3.oas.annotations.media;
+    exports io.swagger.v3.oas.annotations.parameters;
+    exports io.swagger.v3.oas.annotations.responses;
+    exports io.swagger.v3.oas.annotations.security;
+    exports io.swagger.v3.oas.annotations.servers;
+    exports io.swagger.v3.oas.annotations.tags;
+}
